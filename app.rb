b2e6e10440d18b23f4965be072e12e0ff138562a
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-03 18:40:57 +0330

    Update app.rb
    serve index.html static file as response

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App
       
       return [200, headers, ['favicon alt text XD']] if env['PATH_INFO']=='/favicon.ico'
 
-      response = ['<h1>Welcome to Simple Web Application</h1>']
-      
+      response_html = File.read 'views/index.html'
+      response = [response_html]
       [200, headers, response]
     end
   end
